d372e0c908ac6e5db07654db7a7526f8c3565ce2
zlmturnout <turnout@aliyun.com>
2023-02-18 22:15:52 +0800

    h5ToHDF5

--- STXMData_read.py
+++ STXMData_read.py
@@ -30,7 +30,7 @@ def read_h5(filepath:str,main_key:str="FPGA control board"):
     #corrected_counts=median_filter(matrix_counts,5)
     
     #method 3 correct backround by substraction and then median_filter
-    corrected_counts=matrix_counts+matrix_ref.mean()-matrix_ref
+    #corrected_counts=matrix_counts+matrix_ref.mean()-matrix_ref
     #corrected_counts=median_filter(corrected_counts,3)
     
     print(corrected_counts)
@@ -39,7 +39,7 @@ def read_h5(filepath:str,main_key:str="FPGA control board"):
     idx_x=np.argsort(pos_x)
     #print(pos_x,pos_y)
     correct_x=np.array(list(map(lambda x, y: y[x], idx_x, pos_x)))
-    correct_counts=np.array(list(map(lambda x, y: y[x], idx_x, matrix_counts)))
+    correct_counts=np.array(list(map(lambda x, y: y[x], idx_x, matrix_counts)))# wrong pos
     # print(correct_counts)
     # print(matrix_counts)
     #interpolate 
@@ -65,32 +65,62 @@ def read_hdf5(filepath:str):
     
     return h5_file
 
+def reshape_SSRFh5(filepath:str,main_key:str="FPGA control board"):
+    h5_file=h5py.File(filepath,'r')
+    print(h5_file.filename)
+    # for key in h5_file[main_key].keys():
+    #     print(f'key:{key}')
+    h5_data=h5_file.get(main_key)
+    matrix_counts = np.array(h5_data['PMT counter'],dtype=np.float32)
+    row_n,col_n=matrix_counts.shape
+    print(f'Matrix with shape:{matrix_counts.shape}')
+    matrix_ref=np.array(h5_data['PMT ref'],dtype=np.float32)
+    pos_x=np.array(h5_data['positon 1 data'],dtype=np.float32)
+    pos_y=np.array(h5_data['positon 2 data'],dtype=np.float32)
+    # plot historm
+    #plt.hist(matrix_counts)
+    #interpolate 
+    #interp_counts=interpolate.Rbf(pos_x,pos_y,matrix_counts,function='multiquadric')
+    max_x,min_x=pos_x.max(),pos_x.min()
+    max_y,min_y=pos_y.max(),pos_y.min()
+    newpos_x=np.linspace(min_x,max_x,col_n)
+    newpos_y=np.linspace(min_y,max_y,row_n)
+    grid_X,grid_Y=np.meshgrid(newpos_x,newpos_y)
+    # interpolate into new_data with shape(row_n,col_n) in a ordered position
+    
+    orderPMT_counts=griddata((pos_x.flatten(),pos_y.flatten()),matrix_counts.flatten(),(grid_X,grid_Y),method='cubic',fill_value=matrix_counts.mean())
+    orderPMT_ref=griddata((pos_x.flatten(),pos_y.flatten()),matrix_ref.flatten(),(grid_X,grid_Y),method='cubic',fill_value=matrix_counts.mean())
+    return h5_file[main_key],orderPMT_counts,orderPMT_ref
 
 if __name__=="__main__":
     #test_h5=os.path.abspath("h5_data\\SSF20221202080751interp.hdf5")
+    preE_h5file=os.path.abspath("h5_data\\dual_energy\\SF20230115235307.h5")
     
+    mainE_h5file=os.path.abspath("h5_data\\dual_energy\\SF20230115235856.h5")
     test_h5=os.path.abspath("h5_data\\SF20221202061732.h5")
-    #test_h5=os.path.abspath("h5_data\\SF20221205224716.h5")
     
-    h5_data,correct_x,correct_counts,new_counts=read_h5(test_h5)
-    Image_count=h5_data['PMT counter'][:]
+    topup_dotted_h5=os.path.abspath("h5_data\\SF20221205224716.h5")
+    print(os.path.splitext(test_h5))
+    #test_h5=os.path.abspath("h5_data\\SF20230115235856.h5")
+    #h5_file=h5py.File(test_h5,'r')
+    
+    #h5_data,correct_x,correct_counts,new_counts=read_h5(test_h5)
+    h5_data,order_PMTcount,order_PMTref=reshape_SSRFh5(topup_dotted_h5)
+    # original data in sepearted and disordered position
+    Image_count=np.array(h5_data['PMT counter'][:])
+    Image_ref=np.array(h5_data['PMT ref'][:])
+    Image_corrected=Image_count+Image_ref.mean()-Image_ref
     #print(f'original:\n{Image_count}')
     row_n,col_n=Image_count.shape
     
-    # for i in range(row_n):
-    #     if i%2==1:
-    #         line=Image_count[i].copy()
-    #         for j in range(col_n):
-    #             Image_count[i][j]=line[col_n-j-1]
-    #             #new_line.append(line[row_n-j-1])
-    # print(f'new:\n{Image_count}')
-    Image_ref=h5_data['PMT ref']
     Position_1=h5_data['positon 1 data']
     Position_2=h5_data['positon 2 data']
     matrix_count = np.array(Image_count,dtype=np.float32)
     matrix_ref = np.array(Image_ref,dtype=np.float32)
     matrix_pos1 = np.array(Position_1,dtype=np.float32)
     matrix_pos2 = np.array(Position_2,dtype=np.float32)
+
+    # plot part
     fig1 = plt.figure(figsize =(16, 9))
     fig1.canvas.manager.window.setWindowTitle("Visualize raw image")
     
@@ -98,12 +128,15 @@ if __name__=="__main__":
     plt.subplot(2,2,1),plt.scatter(x=matrix_pos1,y=matrix_pos2,c=Image_count,s=10,cmap=cm.rainbow)
     plt.colorbar(location='right', fraction=0.1),plt.title("PMT counter")
     
-    plt.subplot(2,2,2),plt.imshow(new_counts,cmap=cm.rainbow),plt.title("new counts")
+    #plt.subplot(2,2,2),plt.imshow(order_PMTcount,cmap=cm.rainbow),plt.title("order PMT counts")
+    plt.subplot(2,2,2),plt.scatter(x=matrix_pos1,y=matrix_pos2,c=Image_corrected,s=10,cmap=cm.rainbow)
+    plt.colorbar(location='right', fraction=0.1),plt.title("PMT ref")
     #plt.subplot(2,2,2),plt.scatter(x=matrix_pos1,y=matrix_pos2,c=Image_ref,s=10,cmap=cm.rainbow),plt.title("PMT ref")
+    #plt.colorbar(location='right', fraction=0.1)
+    plt.subplot(2,2,3),plt.imshow(order_PMTcount,cmap=cm.rainbow),plt.title("order PMT cpunts")
+    #plt.subplot(2,2,3),plt.imshow(correct_x[1::2],cmap=cm.rainbow),plt.title("Pos X_odd")
     plt.colorbar(location='right', fraction=0.1)
-    plt.subplot(2,2,3),plt.imshow(correct_x[1::2],cmap=cm.rainbow),plt.title("Pos X_odd")
-    plt.colorbar(location='right', fraction=0.1)
-    plt.subplot(2,2,4),plt.imshow(correct_x[::2],cmap=cm.rainbow),plt.title("Pos X_even")
+    plt.subplot(2,2,4),plt.imshow(order_PMTref,cmap=cm.rainbow),plt.title("order PMT ref")
     #plt.subplot(2,2,4),plt.imshow(matrix_pos2,cmap=cm.rainbow),plt.title("PMT Y")
     plt.colorbar(location='right', fraction=0.1)
     # #figure pos line
@@ -117,7 +150,7 @@ if __name__=="__main__":
     fig3 = plt.figure(figsize =(16, 9))
     fig3.canvas.manager.window.setWindowTitle("Visualize raw image")
     #plt.subplot(1,1,1),plt.scatter(x=matrix_pos1,y=matrix_pos2,c=Image_count,s=10,cmap=cm.rainbow)
-    plt.subplot(1,1,1),plt.imshow(new_counts,cmap=cm.rainbow),plt.title("new counts")
+    plt.subplot(1,1,1),plt.imshow(order_PMTcount,cmap=cm.plasma),plt.title("PMT counts")
     plt.colorbar(location='right', fraction=0.1),plt.title("all counts")
     plt.show()
 
---
+++ analog_clock.py
@@ -0,0 +1,138 @@
+# importing libraries
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6 import QtCore, QtGui
+from PySide6.QtGui import Qt, QPolygon, QPainter, QBrush, QPen
+from PySide6.QtCore import QTimer, QPoint, QTime,QRect
+import sys
+
+# creating a clock class
+
+
+class Clock(QMainWindow):
+
+    # constructor
+    def __init__(self):
+        super().__init__()
+
+        # creating a timer object
+        timer = QTimer(self)
+
+        # adding action to the timer
+        # update the whole code
+        timer.timeout.connect(self.update)
+
+        # setting start time of timer i.e 1 second
+        timer.start(1000)
+
+        # setting window title
+        self.setWindowTitle('Clock')
+
+        # setting window geometry
+        self.setGeometry(200, 200, 300, 300)
+
+        # setting background color to the window
+        self.setStyleSheet("background : cyan;")
+
+        # creating hour hand
+        self.hPointer = QtGui.QPolygon([QPoint(6, 7),
+                                        QPoint(-6, 7),
+                                        QPoint(0, -50)])
+
+        # creating minute hand
+        self.mPointer = QPolygon([QPoint(6, 7),
+                                  QPoint(-6, 7),
+                                  QPoint(0, -70)])
+
+        # creating second hand
+        self.sPointer = QPolygon([QPoint(1, 1),
+                                  QPoint(-1, 1),
+                                  QPoint(0, -90)])
+        # colors
+        # color for minute and hour hand
+        self.bColor = Qt.magenta
+
+        # color for second hand
+        self.sColor = Qt.yellow
+
+    # method for paint event
+    def paintEvent(self, event):
+
+        # getting minimum of width and height
+        # so that clock remain square
+        rec = min(self.width(), self.height())
+
+        # getting current time
+        tik = QTime.currentTime()
+
+        # creating a painter object
+        painter = QPainter(self)
+
+        # method to draw the hands
+        # argument : color rotation and which hand should be pointed
+
+        def drawPointer(color, rotation, pointer):
+
+            # setting brush
+            painter.setBrush(QBrush(color))
+
+            # saving painter
+            painter.save()
+
+            # rotating painter
+            painter.rotate(rotation)
+
+            # draw the polygon i.e hand
+            painter.drawConvexPolygon(pointer)
+
+            # restore the painter
+            painter.restore()
+
+        # tune up painter
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        # translating the painter
+        painter.translate(self.width() / 2, self.height() / 2)
+
+        # scale the painter
+        painter.scale(rec / 200, rec / 200)
+
+        # set current pen as no pen
+        painter.setPen(QtCore.Qt.NoPen)
+
+        # draw each hand
+        drawPointer(
+            self.bColor, (30 * (tik.hour() + tik.minute() / 60)), self.hPointer)
+        drawPointer(
+            self.bColor, (6 * (tik.minute() + tik.second() / 60)), self.mPointer)
+        drawPointer(self.sColor, (6 * tik.second()), self.sPointer)
+
+        # drawing background
+        painter.setPen(QPen(self.bColor))
+
+        # for loop
+        for i in range(0, 60):
+
+            # drawing background lines
+            if (i % 5) == 0:
+                painter.drawLine(80, 0, 90, 0)
+                rectangle=QRect(-10,-80,20,20)
+                num=12 if i/5==0 else int(i/5)
+                painter.drawText(rectangle,Qt.Alignment.AlignCenter,str(num))
+            # rotating the painter
+            painter.rotate(6)
+
+        # ending the painter
+        painter.end()
+
+
+# Driver code
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # creating a clock object
+    win = Clock()
+
+    # show
+    win.show()
+
+    exit(app.exec_())
